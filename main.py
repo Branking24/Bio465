6c2c3048a4cc971d08b0ade5cec51c23d93da82e
Branden Kinghorn <brando3034king@gmail.com>
2023-03-01 10:40:39 -0700

    Adding .gitignore, General step comments, seaborn plot example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # model.csv : cols 1 and 9
 def main():
@@ -10,6 +12,24 @@ def main():
 
     new_merge = new_merge.replace(0, pd.np.nan) # data normalization --> getting rid of 0s
     grouped = new_merge.groupby("DepmapModelType").mean()
+
+    #Get mean based on gene
+
+
+
+    #Create Kernel Density Plots
+    #plot = sns.histplot(data=new_merge.iloc[:, 1], kde=True)
+    #plt.show()
+
+    standard = grouped.std()
+
+
+    #Per Cell Type, Find Relative Relationship from Kernel Density Plots
+
+
+    #Compare Training Cells to Gene-Gene Matrices
+
+    #Repeat for cell lines
     return
 
 if __name__ == '__main__':
