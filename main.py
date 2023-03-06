637b11dff9aebf0112da0efb5441f75dfaa6a479
Branden Kinghorn <brando3034king@gmail.com>
2023-03-06 10:26:43 -0700

    First Attempt at Gene to Gene Matrix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import seaborn as sns
 
 # model.csv : cols 1 and 9
 def main():
+    
     omics_df = pd.read_csv("OmicsExpressionProteinCodingGenesTPMLogp1.csv")
     omics_df.columns.values[0] = "ModelID"
 
@@ -25,7 +26,28 @@ def main():
 
 
     #Per Cell Type, Find Relative Relationship
+    relations = {}
+    final = {}
+    for i_type in range(len(grouped.axes[0])):
+        type = grouped.axes[0][i_type]
+        relations[type] = []
+        final[type] = {}
+        for gene in range(len(mean_df.axes[0])):
+            if grouped.iloc[i_type, gene] > mean_df.iloc[gene] + standard.iloc[gene]:
+                relations[type].append(1)
+            elif grouped.iloc[i_type, gene] < mean_df.iloc[gene] - standard.iloc[gene]:
+                relations[type].append(-1)
+            else:
+                relations[type].append(0)
 
+        for i in range(len(relations[type])):
+            for j in range(i, len(relations[type])):
+                if relations[type][i] == 1 and relations[type][j] == -1:
+                    final[type][str(i) + str(j)] = -1
+                elif relations[type][i] == -1 and relations[type][j] == 1:
+                    final[type][str(i) + str(j)] = 1
+                else:
+                    final[type][str(i) + str(j)] = 0
 
     #Compare Training Cells to Gene-Gene Matrices
 
