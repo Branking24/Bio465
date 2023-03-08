1c95d0d99f4dda42b52e8907e7fca0a57da1af1a
Branden Kinghorn <brando3034king@gmail.com>
2023-03-08 10:06:23 -0700

    Finishing first pass at full comparison

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import spearmanr
 
 
 def create_combined_data_frames(data_name, model_name, merge_column_name, column_id):
@@ -24,6 +25,47 @@ def density_plot(to_plot, index):
     plot = sns.histplot(data=to_plot.iloc[:, index], kde=True)
     plot.show()
 
+def compare_dataset(csv_name, csv_model_name, column_id, merge_column_name, standard_deviation, means, relationship_matrix):
+    training_df = pd.read_csv(csv_name)
+    training_df.columns.values[0] = column_id
+    training_model_df = pd.read_csv(csv_model_name, usecols=[column_id, merge_column_name])
+
+    merged = pd.merge(training_df, training_model_df)
+    final = []
+    cur_relation = []
+    relationship = {}
+    for i in range(len(merged.axes[0])):
+        cur_relation = []
+        relationship[merged.axes[0][i]] = []
+        for j in range(1, len(merged.iloc[i].axes[0]) - 1):
+            cur_val = merged.iloc[i].iloc[j]
+            if cur_val == 0:
+                relationship[merged.axes[0][i]].append(0)
+            elif cur_val > (means.iloc[j - 1] + standard_deviation.iloc[j - 1]):
+                relationship[merged.axes[0][i]].append(1)
+            elif cur_val < (means.iloc[j - 1] - standard_deviation.iloc[j - 1]):
+                relationship[merged.axes[0][i]].append(-1)
+            else:
+                relationship[merged.axes[0][i]].append(0)
+
+        for l in range(len(relationship[merged.axes[0][i]])):
+            for j in range(l, len(relationship[merged.axes[0][i]])):
+                if relationship[merged.axes[0][i]][l] == 1 and relationship[merged.axes[0][i]][j] == -1:
+                    cur_relation.append(-1)
+                elif relationship[merged.axes[0][i]][l] == -1 and relationship[merged.axes[0][i]][l] == 1:
+                    cur_relation.append(1)
+                else:
+                    cur_relation.append(0)
+
+        max = 0
+        max_s = ""
+        for k in relationship_matrix.keys():
+            cur, p = spearmanr(relationship_matrix[k], cur_relation)
+            if abs(cur) > max:
+                max = abs(cur)
+                max_s = k
+        final.append(max_s)
+    return
 
 # model.csv : cols 1 and 9
 def main():
@@ -52,7 +94,7 @@ def main():
     for i_type in range(len(grouped.axes[0])):
         type = grouped.axes[0][i_type]
         relations[type] = []
-        final[type] = {}
+        final[type] = []
         for gene in range(len(mean_df.axes[0])):
             if grouped.iloc[i_type, gene] > mean_df.iloc[gene] + standard.iloc[gene]:
                 relations[type].append(1)
@@ -64,15 +106,15 @@ def main():
         for i in range(len(relations[type])):
             for j in range(i, len(relations[type])):
                 if relations[type][i] == 1 and relations[type][j] == -1:
-                    final[type][str(i) + str(j)] = -1
+                    final[type].append(-1)
                 elif relations[type][i] == -1 and relations[type][j] == 1:
-                    final[type][str(i) + str(j)] = 1
+                    final[type].append(1)
                 else:
-                    final[type][str(i) + str(j)] = 0
+                    final[type].append(0)
 
     #Compare Training Cells to Gene-Gene Matrices
-
-
+    predict = compare_dataset("Training.csv", "Model_training.csv", "ModelID", "DepmapModelType", standard, mean_df, final)
+    print(predict)
     #Repeat for cell lines
 
 
