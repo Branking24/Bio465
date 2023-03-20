87c5d04ed9c7759c19a9cb09116513db84afc07e
bstone42 <benstone34@gmail.com>
2023-03-20 10:18:07 -0600

    Modified figure_2_plots.py to work with brain data

--- figures/figure_2_plots.py
+++ figures/figure_2_plots.py
@@ -3,29 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import seaborn as sns
-from processed_data.load_cancer_data import load_cancer_data
+# from processed_data.load_cancer_data import load_cancer_data
+from processed_data.load_brain_data import load_brain_data
 
 
 def plot_figures(*argv):
-    omics_df, model_df, new_merge, grouped, mean_df, standard = load_cancer_data(
-        argv[0], argv[1], "DepmapModelType", "ModelID")
+    gene1 = "C1orf112"
+    gene2 = "TNMD"
+    # TSPAN6, TNMD, DPM1 SCYL3, C1orf112, FGR, CFH
+    # omics_df, model_df, new_merge, grouped, mean_df, standard = load_cancer_data(
+    #     argv[0], argv[1], "DepmapModelType", "ModelID")
+    omics_df, new_merge, grouped, mean_df, standard = load_brain_data(
+        argv[0])
     to_plot = omics_df.iloc[:, 4]
     to_plot_2 = to_plot[to_plot != 0]
-    plot = sns.histplot(data=to_plot_2, kde=True, color=mcolors.CSS4_COLORS["lightsteelblue"])
-    plt.axvline(mean_df.loc["SCYL3 (57147)"], color=mcolors.TABLEAU_COLORS["tab:red"])
-    plt.axvline(mean_df.loc["SCYL3 (57147)"] - standard.loc["SCYL3 (57147)"], color=mcolors.TABLEAU_COLORS["tab:red"], dashes=[5, 2, 0, 0])
-    plt.axvline(mean_df.loc["SCYL3 (57147)"] + standard.loc["SCYL3 (57147)"], color=mcolors.TABLEAU_COLORS["tab:red"], dashes=[5, 2, 0, 0])
-    to_plot = new_merge.loc[new_merge["DepmapModelType"] == "AMLGATA2MECOM"]
-    to_plot_2 = to_plot.iloc[:, 4]
-    to_plot_3 = to_plot_2[to_plot_2 != 0]
-    plot_2 = sns.histplot(data = to_plot_3,kde=False, color=mcolors.CSS4_COLORS["moccasin"])
+    plot = sns.histplot(data=to_plot_2, kde=True, color=mcolors.CSS4_COLORS["lightsteelblue"], stat="density")
+    plt.axvline(mean_df.loc[gene1], color=mcolors.TABLEAU_COLORS["tab:red"])
+    plt.axvline(mean_df.loc[gene1] - standard.loc[gene1], color=mcolors.TABLEAU_COLORS["tab:red"], dashes=[5, 2, 0, 0])
+    plt.axvline(mean_df.loc[gene1] + standard.loc[gene1], color=mcolors.TABLEAU_COLORS["tab:red"], dashes=[5, 2, 0, 0])
+    # to_plot = new_merge.loc[new_merge["DepmapModelType"] == "AMLGATA2MECOM"]
+    # to_plot_2 = to_plot.iloc[:, 4]
+    # to_plot_3 = to_plot_2[to_plot_2 != 0]
+    # plot_2 = sns.histplot(data = to_plot_3,kde=False, color=mcolors.CSS4_COLORS["moccasin"])
     plt.show()
-    to_plot = omics_df.iloc[:, 3]
+    to_plot = omics_df.iloc[:, 1]
     to_plot_2 = to_plot[to_plot != 0]
-    plot = sns.histplot(data=to_plot_2, kde=True, color=mcolors.CSS4_COLORS["lightsteelblue"])
-    plt.axvline(mean_df.loc["DPM1 (8813)"], color=mcolors.TABLEAU_COLORS["tab:red"])
-    plt.axvline(mean_df.loc["DPM1 (8813)"] - standard.loc["DPM1 (8813)"], color=mcolors.TABLEAU_COLORS["tab:red"], dashes=[5, 2, 0, 0])
-    plt.axvline(mean_df.loc["DPM1 (8813)"] + standard.loc["DPM1 (8813)"], color=mcolors.TABLEAU_COLORS["tab:red"], dashes=[5, 2, 0, 0])
+    plot = sns.histplot(data=to_plot_2, kde=True, color=mcolors.CSS4_COLORS["lightsteelblue"], stat="density")
+    plt.axvline(mean_df.loc[gene2], color=mcolors.TABLEAU_COLORS["tab:red"])
+    plt.axvline(mean_df.loc[gene2] - standard.loc[gene2], color=mcolors.TABLEAU_COLORS["tab:red"], dashes=[5, 2, 0, 0])
+    plt.axvline(mean_df.loc[gene2] + standard.loc[gene2], color=mcolors.TABLEAU_COLORS["tab:red"], dashes=[5, 2, 0, 0])
     plt.show()
 
-plot_figures("OmicsExpressionProteinCodingGenesTPMLogp1.csv", "Model.csv")
+# plot_figures("OmicsExpressionProteinCodingGenesTPMLogp1.csv", "Model.csv")
+plot_figures("../data/GSE181153_ADAB_geneCounts.tsv")
--- processed_data/load_brain_data.py
+++ processed_data/load_brain_data.py
@@ -1,17 +1,35 @@
 import pandas as pd
 import regex as re
+import numpy as np
 
-data = "../data/GSE181153_ADAB_geneCounts.tsv"
-out_filepath = "../data/processed_brain_data.csv"
+def load_brain_data(data):
+    # data = "../data/GSE181153_ADAB_geneCounts.tsv"
+    # out_filepath = "../data/processed_brain_data.csv"
 
-data_df = pd.read_csv(data, sep='\t')
+    data_df = pd.read_csv(data, sep='\t')
 
-data_df = data_df.drop('ensembl', axis=1)
+    data_df = data_df.drop('ensembl', axis=1)
 
-renamed_data = data_df.rename(columns=lambda x: re.sub('ADAB_RNA_','',x))
-renamed_data = renamed_data.rename(columns=lambda x: re.sub('_.*','',x))
+    renamed_data = data_df.rename(columns=lambda x: re.sub('ADAB_RNA_','',x))
+    renamed_data = renamed_data.rename(columns=lambda x: re.sub('_.*','',x))
 
-data_transpose = renamed_data.transpose()
+    data_transpose = renamed_data.transpose()
+    data_transpose.columns = data_transpose.iloc[0]
+    data_transpose = data_transpose.drop(['hgnc'])
 
-data_transpose.to_csv(out_filepath)
+    # data_transpose.to_csv(out_filepath)
+
+    # data_df.columns.values[0] = column_id
+
+    # model_df = pd.read_csv(model_name, usecols=[column_id, merge_column_name])
+    # new_merge = pd.merge(data_df, model_df)
+
+    new_merge = data_transpose.replace(0, np.nan) # data normalization --> getting rid of 0s
+    grouped = new_merge.groupby(level=0).mean()
+
+    mean_df = grouped.mean()
+
+    standard_df = grouped.std()
+
+    return data_transpose, new_merge, grouped, mean_df, standard_df
 
