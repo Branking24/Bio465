cd7f0a261f9913d51b6b5e8fd46ece02e3f7f8eb
Branden Kinghorn <brando3034king@gmail.com>
2023-03-22 10:48:10 -0600

    Trying to stream csv from box

--- processed_data/load_cancer_data.py
+++ processed_data/load_cancer_data.py
@@ -1,18 +1,25 @@
 import pandas as pd
 import numpy as np
+import boxsdk as box
 
-def load_cancer_data(data_name, model_name, merge_column_name, column_id):
-    data_df = pd.read_csv(data_name)
-    data_df.columns.values[0] = column_id
+def load_cancer_data():
+    config = box.JWTAuth.from_settings_file("../config/config.json")
+    client = box.Client(config)
+    omics_id = '1169438801565'
+    model_id = '1169436718115'
+    data_df = pd.read_csv(client.file(omics_id).content())
+    data_df.columns.values[0] = "ModelID"
 
-    model_df = pd.read_csv(model_name, usecols=[column_id, merge_column_name])
+    model_df = pd.read_csv(client.file(model_id).content(), usecols=["ModelID", "DepmapModelType"])
     new_merge = pd.merge(data_df, model_df)
 
     new_merge = new_merge.replace(0, np.nan) # data normalization --> getting rid of 0s
-    grouped = new_merge.groupby(merge_column_name).mean()
+    grouped = new_merge.groupby("DepmapModelType").mean()
 
     mean_df = grouped.mean()
 
     standard_df = grouped.std()
 
     return data_df, model_df, new_merge, grouped, mean_df, standard_df
+
+load_cancer_data()
